4f2a953b6a0ecd02af9d6b5daf605bf88d9da91a
Axel Masson <axel.masson.pro@gmail.com>
2018-03-22 10:54:47 +0100

    Functions integration

--- OW Alexa/app/app.js
+++ OW Alexa/app/app.js
@@ -6,6 +6,8 @@
 
 const {App} = require('jovo-framework');
 var request = require('request');
+var EventEmitter = require("events").EventEmitter;
+var playerProfile = new EventEmitter(); 
 
 const config = {
     logging: true,
@@ -34,33 +36,67 @@ app.setHandler({
         let speech = "";
         let reprompt = "";
         let user = specificPlayer.value + "-" + number.value;
-        let newNumber = 2975;
-        let aaver = "ELOAssassin-" + newNumber;
-        let API = "https://ow-api.com/v1/stats/pc/eu/"+aaver +"/profile";
+        
+        let API = "https://ow-api.com/v1/stats/pc/eu/"+ user +"/profile";
         let alexa = this.alexaSkill();
         console.log(user);
         console.log(API);
-
-        request(API, function(error, response, body){
-            
-            var playerProfile = JSON.parse(body);
-            console.log(playerProfile);
-            var totalCompetitiveGames = playerProfile["competitiveStats"]["games"]["played"];
-            console.log(totalCompetitiveGames);
-            var wonGames = playerProfile["competitiveStats"]["games"]["won"];
-            console.log(wonGames);
-            var loosedGames = (totalCompetitiveGames - wonGames);
-            var ratio = (wonGames / loosedGames);
-
-            speech = "Le joueur" + aaver + "totalise " +wonGames + "de partie gagnées"
-            + "et " + loosedGames +  "de parties perdues, soit un ratio de victoire égale à"
-            + ratio;
-
-            alexa.tell(speech);
-        });
         
 
+        //Getting all infos
+        request(API, function(error, response, body){
+            playerProfile.data  = JSON.parse(body);
+            playerProfile.emit("retrieve");
+        });
+
+        playerProfile.on('retrieve', function () {
+
+            console.log(playerProfile.data);
+
+            if(specificStats.value == "le ratio de victoire"){
+                console.log("Ratio choisis");
+                speech = GetVictoryRatio(playerProfile.data);
+                alexa.tell(speech);
+            }
+            
+            if (specificStats.value == "le niveau"){
+                console.log("Niveau choisi");
+                speech = GetLevel(playerProfile.data);
+                alexa.tell(speech);
+            }
+
+        });
     }
 });
+
+
+//Getting victory ratio 
+function GetVictoryRatio(playerInfos){
+
+    console.log(playerInfos);
+    var totalCompetitiveGames = playerInfos["competitiveStats"]["games"]["played"];
+    var wonGames = playerInfos["competitiveStats"]["games"]["won"];
+    var loosedGames = (totalCompetitiveGames - wonGames);
+    var finalRatio = (wonGames / loosedGames);
+    var playerName = playerInfos["name"];
+
+    let speech = "Le joueur " + playerName + " totalise " + wonGames + " parties gagnées"
+    + " et " + loosedGames + " parties perdues, soit un ratio de victoire égale à "
+    + finalRatio;
+    console.log(speech);
+    return speech; 
+}
+
+//Retrieing player lvl and prestige
+function GetLevel(playerInfos){
+
+    var prestigePlayer = playerInfos["prestige"];
+    var lvlPlayer = playerInfos["level"];
+    var playerName = playerInfos["name"];
+
+    let speech = "Le joueur " + playerName + " est actuellement prestige  " + prestigePlayer + " et est de niveau " + lvlPlayer;
+    console.log(speech);
+    return speech;
+}
 
 module.exports.app = app;
